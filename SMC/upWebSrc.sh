bba2e1049e0f4e2064ce04e2f1d6bc1c0a0d9cfa
Jerry Shih <jerryshih@supermicro.com.tw>
2020-06-19 13:35:42 +0800

    Add webui src import script

---
+++ SMC/upWebSrc.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+#   Author: JerryShih
+#   Mail: JerryShih@supermicro.com
+#   Usage:
+#       [Only x12 need new WebGUI]
+#       1. navigate to x12 codebase
+#       2. upWebSrc.sh
+
+SRCPATCH=$(pwd)
+WEBSRC=/home/jerry/codebase/Pure/webgui_gen2
+WEBPATCH=$SRCPATCH/Web_Server/web/SUPERMICRO
+
+echo ""
+echo "Update [$WEBSRC] to [$WEBPATCH]"
+echo ""
+
+if [ $# -ge 1 ]; then
+    echo "The script no need arguments!"
+    exit
+fi
+
+if [ -d $WEBSRC ]; then
+    cd $WEBSRC
+    echo "Update web source git..."
+    git pull
+    echo "Already update to HEAD!!"
+    echo ""
+else
+    echo "[$WEBSRC] incorrect"
+    exit
+fi
+
+if [ -d $WEBPATCH ]; then
+    cd $WEBPATCH
+    echo "Remove and copy..."
+    rm -r css/ js/ page/ images/
+    cp -r $WEBSRC/css/ $WEBSRC/js/ $WEBSRC/page/ $WEBSRC/images/ $WEBPATCH/
+    echo "Already copy to HEAD!!"
+    echo ""
+else
+    echo "[$WEBPATCH] incorrect"
+    exit
+fi
