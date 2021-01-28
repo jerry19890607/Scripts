7f04b055e23cc5256cad7f110d00bdbf581b7d26
Jerry Shih <jerryshih@supermicro.com.tw>
2021-01-28 18:22:49 +0800

    Add key

--- SMC/autoBuildSMC.sh
+++ SMC/autoBuildSMC.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 codebase=$1
+key=$2
 build_date="$(date +%Y%m%d)"
 codebase_root=/home/jerry/ssd/codebase
 folder_name="autobuild_$build_date"
@@ -10,7 +11,14 @@ build_script_file=/home/jerry/codebase/Pure/forAutoBuild
 build_script="aa_$codebase.sh"
 build_patch=$codebase"_build.patch"
 
-if [ "$#" -ne 1 ]; then
+usage () {
+    echo -e "autoBuildSMC.sh usage:"
+    echo -e "   $0 [x11|x12] [d|p]"
+    echo ""
+}
+
+if [ "$#" -ne 1 ] && [ "$#" -ne 2 ]; then
+    usage
     echo "Input parameters ERROR!"
     exit
 fi
@@ -19,6 +27,7 @@ if [ $(uname -i) == "x86_64" ]; then
     echo "AST2600 build code server"
     if [ $codebase != "x12" ]; then
         echo "This build code server(x64) only support x12 codebase"
+        usage
         exit
     fi
     build_machine=2600
@@ -26,11 +35,17 @@ elif [ $(uname -i) == "i686" ]; then
     echo "AST2500 build code server"
     if [ $codebase != "x12" ] && [ $codebase != "x11" ]; then
         echo "This build code server(x32) only support x11 or x12 codebase"
+        usage
         exit
     fi
     build_machine=2500
 else
     echo "Get build code server env ERROR!!"
+    exit
+fi
+
+if [ -d $codebase_root/$folder_name/$codebase_name ]; then
+    echo "$codebase_name already exists!!"
     exit
 fi
 
@@ -69,13 +84,23 @@ if [ $build_machine -eq 2500 ]; then
         echo "Git apply $build_patch ERROR!!"
         exit
     fi
+else
+    if [ $key == "d" ]; then
+        sed -i 's/\(PRODUC_KEY=\).*/\10/' $build_script
+    elif [ $key == "p" ]; then
+        sed -i 's/\(PRODUC_KEY=\).*/\11/' $build_script
+    else
+        echo "Key type ERROR!!"
+        exit
+    fi
 fi
+
 sh $build_script
+if [ $? -ne 0 ]; then
+    echo "Build ERROR!!"
+else
+    echo "Build success!!"
+fi
 
 <<'###BLOCK-COMMENT'
 ###BLOCK-COMMENT
-
-
-
-
-
