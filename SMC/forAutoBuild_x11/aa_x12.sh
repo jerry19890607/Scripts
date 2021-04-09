7af0c357d6f7eaf77ca6e2c54e6368b3c070fc65
Jerry Shih <jerryshih@supermicro.com.tw>
2021-04-09 11:30:42 +0800

    Change taget for h12 ast2500

--- SMC/forAutoBuild_x11/aa_x12.sh
+++ SMC/forAutoBuild_x11/aa_x12.sh
@@ -8,8 +8,13 @@ export SMCIUSR=tonyhuang
 export SMCIPWD=tH20210311
 
 cacheclean.sh &&
-make sh12_rot_64_rsa4k_std ver=1.0.0 core=8 &&
-make sh12_rot_64_rsa4k_std signfile=./images/AST2500_all.bin
+# old target
+#make sh12_rot_64_rsa4k_std ver=1.0.0 core=8 &&
+#make sh12_rot_64_rsa4k_std signfile=./images/AST2500_all.bin
+
+# new target
+make sh12_rot_ast25_std_p ver=1.0.0 core=8 &&
+make sh12_rot_ast25_std_p signfile=./images/AST2500_all.bin
 
 isbmc2k4k signtmp/signed_AST2600_4675.bin
 
