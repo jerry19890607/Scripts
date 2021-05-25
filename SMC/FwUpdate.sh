a9f907516176c49d0a4a959bc22f8272f3332164
Jerry Shih <jerryshih@supermicro.com.tw>
2021-05-25 13:49:32 +0800

    Finish 3 types update function

--- SMC/FwUpdate.sh
+++ SMC/FwUpdate.sh
@@ -12,7 +12,8 @@ cred="$( echo -n $NAME:$PASSWORD | base64 )"
 
 usage () {
     echo -e "Usage:"
-    echo -e " FwUpdate.sh (bmc|bios) IP_ADDR FW_IMAGE"
+    echo -e ""
+    echo -e "   FwUpdate.sh (bmc|bios|cpld) IP_ADDR FW_IMAGE"
     echo -e ""
 }
 
@@ -22,18 +23,37 @@ if [ $ARGUNUM != 3 ]; then
     exit
 fi
 
-echo [$TYPE][$IP][$cred][$FW_IMAGE]
+if expr "$IP" : '[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*\.[0-9][0-9]*$' >/dev/null; then
+    for i in 1 2 3 4; do
+        if [ $(echo "$IP" | cut -d. -f$i) -gt 255 ]; then
+            echo "Invalid IP ($IP)"
+            echo ""
+            usage
+            exit
+        fi
+    done
+else
+    echo "Invalid IP ($IP)"
+    echo ""
+    usage
+    exit
+fi
+
+echo [$TYPE] [$IP] [$cred] [$FW_IMAGE]
 
 #BMC
 if [ $TYPE == "bmc" ]; then
     curl \
-    -k -X POST \
-    https://$IP/redfish/v1/UpdateService/upload \
+    -k -X POST https://$IP/redfish/v1/UpdateService/upload \
     -H "Authorization: Basic $cred" \
     -H 'cache-control: no-cache' \
     -H 'content-type: multipart/form-data' \
     -F UpdateFile=@//$FW_IMAGE \
-    -F 'UpdateParameters={"Targets":["/redfish/v1/Managers/1"],"@Redfish.OperationApplyTime":" Immediate ","Oem":{"Supermicro": {"BMC": {"PreserveCfg": false,"PreserveSdr": true,"PreserveSsl": false,"BackupBMC": false}}}}'
+    -F 'UpdateParameters={
+            "Targets":["/redfish/v1/Managers/1"],
+            "@Redfish.OperationApplyTime":"Immediate",
+            "Oem":{"Supermicro": {"BMC": {"PreserveCfg": false,"PreserveSdr": true,"PreserveSsl": false,"BackupBMC": false}}}
+       }'
 
     echo ""
     exit;
@@ -42,13 +62,33 @@ fi
 #BIOS
 if [ $TYPE == "bios" ]; then
     curl \
-    -k -X POST \
-    https://$IP/redfish/v1/UpdateService/upload \
+    -k -X POST https://$IP/redfish/v1/UpdateService/upload \
     -H "Authorization: Basic $cred" \
     -H 'cache-control: no-cache' \
     -H 'content-type: multipart/form-data' \
     -F UpdateFile=@//$FW_IMAGE \
-    -F 'UpdateParameters={"Targets":["/redfish/v1/Systems/1/Bios"],"@Redfish.OperationApplyTime":" Immediate ","Oem":{"Supermicro": {"BIOS": {"PreserveME": false,"PreserveNVRAM": true,"PreserveSMBIOS": false,"BackupBIOS": false}}}}'
+    -F 'UpdateParameters={
+            "Targets":["/redfish/v1/Systems/1/Bios"],
+            "@Redfish.OperationApplyTime":"Immediate",
+            "Oem":{"Supermicro": {"BIOS": {"PreserveME": false,"PreserveNVRAM": true,"PreserveSMBIOS": false,"BackupBIOS": false}}}
+        }'
+
+    echo ""
+    exit;
+fi
+
+#CPLD
+if [ $TYPE == "cpld" ]; then
+    curl \
+    -k -X POST https://$IP/redfish/v1/UpdateService/upload \
+    -H "Authorization: Basic $cred" \
+    -H 'cache-control: no-cache' \
+    -H 'content-type: multipart/form-data' \
+    -F UpdateFile=@//$FW_IMAGE \
+    -F 'UpdateParameters={
+            "Targets":["/redfish/v1/UpdateService/FirmwareInventory/CPLD_Motherboard"],
+            "@Redfish.OperationApplyTime":"Immediate"
+        }'
 
     echo ""
     exit;
